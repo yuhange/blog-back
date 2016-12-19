7ebfd6d52dc53393c3b13674b2410e90dc8be1c9
yuhange <w868433@163.com>
2016-12-19 15:09:36 +0800

    add tags and categories

--- routes/tags.js
+++ routes/tags.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 
-router.get('/tags', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var json = {
       data: [
         {
